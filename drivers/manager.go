a12a9bc3e51222ddaf705196936ce6c942382319
agoalofalife <agoalofalife@gmail.com>
2018-07-10 12:21:47 +0300

    add pre structure project

---
+++ drivers/manager.go
@@ -0,0 +1,42 @@
+package drivers
+
+// string names - driver
+const DRIVER_YANDEX = "yandex";
+
+
+/**
+ / Contract is defined type driver and transfers control to the next
+ */
+type DriverDefined interface{
+	define(driver string)
+}
+
+
+type Definer struct {
+	driver Driver
+}
+
+// Define current driver
+func (definer Definer) Define(driver string) Driver {
+	switch driver {
+	 case DRIVER_YANDEX:
+		return &Yandex{}
+	default:
+		panic("Driver is not found!")
+	}
+}
+
+/**
+ / Base methods for implements drivers payouts
+ */
+type Driver interface {
+	// called prior to execution of the payment
+	pre() bool
+	// call after to execution of the payment
+	after() bool
+
+	executePayout()
+
+	// get name Driver
+	getName() string
+}
---
+++ drivers/yandex.go
@@ -0,0 +1,6 @@
+package drivers
+
+
+type Yandex struct {
+
+}
