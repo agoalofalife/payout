a38415dd95f7dc3bb2f86451f0035082597bf573
agoalofalife <agoalofalife@gmail.com>
2018-07-19 10:39:04 +0300

    a rough idea for Yandex Driver

--- drivers/yandex.go
+++ drivers/yandex.go
@@ -1,13 +1,204 @@
 package drivers
 
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"os"
+	"crypto/tls"
+	"log"
+	"os/exec"
+	"io"
+	"encoding/xml"
+	"time"
+	"strconv"
+	"bytes"
+)
 
-type Yandex struct {}
+// status response
+const (
+	statusSuccess    = 0
+	statusInProgress = 1
+	statusRejected   = 3
 
+	errorSyntax                   = 10
+	errorAgentId                  = 11
+	errorSubAgentId               = 12
+	errorCurrency                 = 14
+	errorRequestDt                = 15
+	errorDstAccount               = 16
+	errorAmount                   = 17
+	errorClientOrderId            = 18
+	errorContract                 = 19
+	errorForbiddenOperation       = 21
+	errorNotUniqueClientOrderId   = 26
+	errorBrokenPack               = 50
+	errorInvalidSignature         = 51
+	errorUnknowSignCert           = 53
+	errorExpiredCert              = 55
+	errorAccountClosed            = 40
+	errorLockedYaWallet           = 41
+	errorUnknowAccount            = 42
+	errorOnceLimit                = 43
+	errorPeriodLimit              = 44
+	errorSmallBalance             = 45
+	errorAmountTooSmall           = 46
+	errorDepositionRequest        = 48
+	errotLimitReceiverBalance     = 201
+	errorYandexError              = 30
+	errorReceiverRejectDeposition = 31
+	errorPaymentExpiredTime       = 105
+	errorReceiverPaymentRevert    = 110
+)
+
+var descriptionErrors = map[int]string{
+	errorSyntax:                   "Ошибка синтаксического разбора XML-документа. Синтаксис документа нарушен или отсутствуют обязательные элементы XML.",
+	errorAgentId:                  "Отсутствует или неверно задан идентификатор контрагента (agentId).",
+	errorSubAgentId:               "Отсутствует или неверно задан идентификатор канала приема переводов (subagentId).",
+	errorCurrency:                 "Отсутствует или неверно задана валюта (currency).",
+	errorRequestDt:                "Отсутствует или неверно задано время формирования документа (requestDT).",
+	errorDstAccount:               "Отсутствует или неверно задан идентификатор получателя средств (dstAccount).",
+	errorAmount:                   "Отсутствует или неверно задана сумма (amount).",
+	errorClientOrderId:            "Отсутствует или неверно задан номер транзакции (clientOrderId).",
+	errorContract:                 "Отсутствует или неверно задано основание для зачисления перевода (contract).",
+	errorForbiddenOperation:       "Запрашиваемая операция запрещена для данного типа подключения контрагента.",
+	errorNotUniqueClientOrderId:   "Операция с таким номером транзакции (clientOrderId), но другими параметрами уже выполнялась.",
+	errorBrokenPack:               "Невозможно открыть криптосообщение, ошибка целостности пакета.",
+	errorInvalidSignature:         "АСП не подтверждена (данные подписи не совпадают с документом).",
+	errorUnknowSignCert:           "Запрос подписан неизвестным Яндекс.Деньгам сертификатом.",
+	errorExpiredCert:              "Истек срок действия сертификата в системе контрагента.",
+	errorAccountClosed:            "Счет закрыт.",
+	errorLockedYaWallet:           "Кошелек в Яндекс.Деньгах заблокирован. Данная операция для этого кошелька запрещена.",
+	errorUnknowAccount:            "Счета с таким идентификатором не существует.",
+	errorOnceLimit:                "Превышено ограничение на единовременно зачисляемую сумму.",
+	errorPeriodLimit:              "Превышено ограничение на максимальную сумму зачислений за период времени.",
+	errorSmallBalance:             "Недостаточно средств для проведения операции.",
+	errorAmountTooSmall:           "Сумма операции слишком мала.",
+	errorDepositionRequest:        "Ошибка запроса зачисления перевода на банковский счет, карту, мобильный телефон.",
+	errotLimitReceiverBalance:     "Превышен лимит остатка на счете получателя.",
+	errorYandexError:              "Технические проблемы на стороне Яндекс.Денег.",
+	errorReceiverRejectDeposition: "Получатель перевода отклонил платеж (под получателем понимается сотовый оператор или процессинговый банк).",
+	errorPaymentExpiredTime:       "Превышено допустимое время оплаты по данному коду платежа (при оплате наличными через терминалы, салоны связи и пр.).",
+	errorReceiverPaymentRevert:    "Получатель перевода вернул платеж (под получателем понимается сотовый оператор или процессинговый банк).",
+}
+
+type Yandex struct {
+	TypePayout
+}
+
+func (yandex Yandex) verify(data []byte, pathCert string) ([]byte, error) {
+	path, err := exec.LookPath("openssl")
+	if err != nil {
+		log.Fatal("You need to install openssl!")
+		os.Exit(-1)
+	}
+
+	cmd := exec.Command(path, "smime", "-verify", "-inform", "PEM", "-nointern", "-certfile", pathCert, "-CAfile", pathCert)
+	stdin, err := cmd.StdinPipe()
+
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, string(data))
+	}()
+
+	return cmd.CombinedOutput()
+}
 // Get name driver
-func (yandex Yandex) getName() string {
+func (yandex Yandex) GetName() string {
 	return DRIVER_YANDEX
 }
 
-type BalanceRequest struct {
+func (yandex Yandex) ExecutePayout() {
+	host := os.Getenv("YANDEX_MONEY_PAYOUT_HOST")
+	url := host + "/webservice/deposition/api/" + yandex.GetType()
 
+	// Load client cert
+	certificate, err := tls.LoadX509KeyPair(os.Getenv("YANDEX_CERT_PATH"), os.Getenv("YANDEX_PRIVATE_KEY_PATH"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{certificate},
+		InsecureSkipVerify: true,
+	}
+
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+
+	xmlReader := yandex.getDataRequest()
+
+	resp, err := client.Post(url, "application/pkcs7-mime", xmlReader)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, err := yandex.verify(data, os.Getenv("YANDEX_CERT_VERIFY_RESPONSE"))
+
+	log.Println(string(out), "ded", err)
+	os.Exit(0)
+}
+
+// TYPE REQUESTS YANDEX
+
+// get balance
+type BalanceRequest struct {
+	ClientOrderId int // field clientOrderId
+}
+
+// Get data request
+func (request BalanceRequest) getDataRequest() io.Reader {
+	agentId, err := strconv.Atoi(os.Getenv("YANDEX_MONEY_PAYOUT_AGENT_ID"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	currentTime := time.Now()
+
+	baseXml := BaseXml{
+		AgentId:       agentId,
+		ClientOrderId: request.ClientOrderId,
+		RequestDT:     currentTime.Format(`2006-01-02T15:04:05.999Z`),
+	}
+
+	xmlStruct := balanceRequestXml{
+		baseXml,
+	}
+
+	buff := bytes.NewBuffer([]byte(xml.Header))
+	enc := xml.NewEncoder(buff)
+	enc.Indent("  ", "    ")
+
+	if err := enc.Encode(xmlStruct); err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	return buff
+}
+
+func (request BalanceRequest) GetType() string {
+	return "balance"
+}
+
+type BaseXml struct {
+	AgentId       int    `xml:"agentId,attr"`
+	ClientOrderId int    `xml:"clientOrderId,attr"`
+	RequestDT     string `xml:"requestDT,attr"`
+}
+type balanceRequestXml struct {
+	BaseXml
+}
+type MakeDepositionRequestXml struct {
+	MakeDepositionRequest xml.Name `xml:"makeDepositionRequest"`
 }
