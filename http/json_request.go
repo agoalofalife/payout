935e3c5027b3e677c6742d5fbbd2d09823998c38
agoalofalife <agoalofalife@gmail.com>
2018-10-11 15:58:19 +0300

    add json structure payment params

--- http/json_request.go
+++ http/json_request.go
@@ -16,3 +16,54 @@ type DepositionRequest struct {
 func newDepositionJsonRequest()  DepositionRequest{
 	return DepositionRequest{nil, nil, nil, ""}
 }
+
+type DepositionPaymentParams struct {
+	PofOfferAccepted bool
+}
+
+type PaymentParamsPhone struct {
+	DepositionPaymentParams
+	Property1 uint16
+	Property2 uint32
+}
+
+type PaymentParamsBankAccount struct {
+	DepositionPaymentParams
+	CustAccount uint64
+	BankBIK uint32
+	PaymentPurpose string
+	Pdr_lastName string
+	Pdr_firstName string
+	Pdr_middleName string
+	Pdr_docNumber uint64
+	Pdr_docIssueYear uint16
+	Pdr_docIssueMonth uint8
+	Pdr_docIssueDay uint8
+	Pdr_address string
+	PdrBirthDate string
+	SmsPhoneNumber uint64
+
+	BankName string
+	BankCity string
+	BankCorAccount string
+}
+
+type PaymentParamsBankCard struct {
+	DepositionPaymentParams
+	Skr_destinationCardSynonim string
+	Pdr_lastName string
+	Pdr_firstName string
+	Pdr_middleName string
+	Pdr_docNumber uint64
+	Pdr_docIssueYear uint16
+	Pdr_docIssueMonth uint8
+	Pdr_docIssueDay uint8
+	SmsPhoneNumber uint64
+	Pdr_birthDate string
+	Pdr_birthPlace string
+	Pdr_docIssuedBy string
+	Pdr_country string
+	Pdr_city string
+	Pdr_address string
+	Pdr_postcode string
+}
