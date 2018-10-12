bba9608f94d28fd834d592ad7a7488918f133b2a
agoalofalife <agoalofalife@gmail.com>
2018-10-12 15:51:57 +0300

    fix migrate string mysql

--- databases/mysql/mysql.go
+++ databases/mysql/mysql.go
@@ -10,7 +10,7 @@ type Mysql struct {
 func (m Mysql) Migrate() string {
 	return `CREATE TABLE IF NOT EXISTS request (
   id int(11) NOT NULL AUTO_INCREMENT,
-  type_transfer varchar(255) NOT NULL,
+  type_transfer varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   dstAccount varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   clientOrderId bigint(11) NOT NULL,
   requestDT datetime NOT NULL,
